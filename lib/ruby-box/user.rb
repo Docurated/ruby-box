7807f5d9bcc558e000bd7feb9d1da5152f7f1a2a
Ryan <ryan.cooke@gmail.com>
2014-03-30 18:11:18 -0400

    adds generic field fetching for users

--- lib/ruby-box/user.rb
+++ lib/ruby-box/user.rb
@@ -1,16 +1,28 @@
 module RubyBox
   class User < Item
-    
+
     def enterprise
-      resp = @session.get( "#{RubyBox::API_URL}/users/#{id}?fields=enterprise" )
-      resp["enterprise"]
+      reload_field("enterprise")
     end
-    
+
+    def role
+      reload_field("role")
+    end
+
     private
+
+    def reload_field field
+      @fields ||= { }
+      @fields[field] ||= begin
+        resp = @session.get( "#{RubyBox::API_URL}/users/#{id}?fields=#{ field }" )
+        resp[field]
+      end
+      @fields[field]
+    end
 
     def resource_name
       'users'
     end
-    
+
   end
 end
